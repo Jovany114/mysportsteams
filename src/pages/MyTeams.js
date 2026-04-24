b8ede88094ffb11c7031461a7259000b6d91d958
Jovany Martinez <jmart397@uic.edu>
2026-04-24 17:21:59 -0500

    Added followed teams to display in MyTeams

--- src/pages/MyTeams.js
+++ src/pages/MyTeams.js
@@ -1,12 +1,38 @@
 import React from 'react' ;
+import { useTeams } from '../context/TeamsContext';
 
 function MyTeams() {
-    return (
-        <div> 
-            <h2>My Teams</h2> 
-            <h4><br></br>You are not following any teams. followed teams will appear here</h4> {/* using <br> for a newline break */}
-        </div>
-    );
+    const { followedTeams, unfollowTeam } = useTeams();
+
+    if (followedTeams.length === 0){
+        return (
+            <div className="empty-state"> 
+                <p>You're not following any teams yet.</p>
+                <p>Head to a league tab to follow your teams!</p>
+            </div>
+        );
+    } else {
+        return(
+            <div>
+                <h2>⭐ My Teams</h2>
+                <div className="teams-grid">
+                    {followedTeams.map((team) => (
+                        <div key={team.name} className="team-card followed">
+                            <div className="team-abbreviation">{team.abbreviation}</div>
+                            <p className="team-name">{team.name}</p>
+                            <p className="team-league">{team.league}</p>
+                            <button
+                                className="follow-btn"
+                                onClick={() => unfollowTeam(team.name)}
+                            >
+                                ✓
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MyTeams;
